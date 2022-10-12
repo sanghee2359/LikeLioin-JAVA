0b20ae0f2822d3804d988e1b647b7c9e6f75d63e
sanghee <wjdtkdgml7352@daum.net>
2022-10-12 09:34:52 +0900

    First Commit

--- Week4/Date1012/Max01.java
+++ Week4/Date1012/Max01.java
@@ -1,2 +1,25 @@
-package Week4.Date1012;public class Max01 {
+package Week4.Date1012;
+
+import java.util.Scanner;
+
+public class Max01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[9];
+        int max = Integer.MIN_VALUE;
+        int maxIdx = 0;
+
+        for(int i=0; i<9; i++){         // 1차원 배열에 값 입력
+            num[i] = sc.nextInt();
+        }
+
+        for(int i=0; i< num.length; i++){
+            if(max < num[i]) {          // max보다 큰 수이면 대입
+                max = num[i];
+                maxIdx = i+1;           // max일 때 index 값 대입
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxIdx);
+    }
 }
