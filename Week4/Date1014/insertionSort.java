93c0ed2100d6f8d2ad2f82e9ba3cbbcb6de9c92d
sanghee <wjdtkdgml7352@daum.net>
2022-10-14 09:37:19 +0900

    First Commit

--- Week4/Date1014/insertionSort.java
+++ Week4/Date1014/insertionSort.java
@@ -7,7 +7,6 @@ public class insertionSort {
         for(int i = 1; i < arr.length; i++){
             System.out.println();
             System.out.println("focus : arr["+i+"]="+arr[i]);
-            System.out.println("현재"+Arrays.toString(arr));
             for(int j=i-1; j>=0; j--) {
                 System.out.printf("arr["+j+"]="+arr[j]+"\n");
                 if(arr[i] < arr[j]){
