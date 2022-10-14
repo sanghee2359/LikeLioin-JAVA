73c4eeee8631e91e8f998dec3c8b3436fd118638
sanghee <wjdtkdgml7352@daum.net>
2022-10-14 10:00:10 +0900

    [수정] 반복문 수정

--- Week4/Date1014/insertionSort.java
+++ Week4/Date1014/insertionSort.java
@@ -3,21 +3,26 @@ package Week4.Date1014;
 import java.util.Arrays;
 
 public class insertionSort {
-    public void sort(int[] arr){
-        for(int i = 1; i < arr.length; i++){
-            System.out.println();
-            System.out.println("focus : arr["+i+"]="+arr[i]);
-            for(int j=i-1; j>=0; j--) {
-                System.out.printf("arr["+j+"]="+arr[j]+"\n");
-                if(arr[i] < arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    System.out.println("swap"+Arrays.toString(arr));
-                }
+    /*public void swap(int[]arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        System.out.println("swap");
+    }*/
+    public void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j;
+            int temp = arr[i];
+            System.out.println("focus : arr[" + i + "]=" + temp);
+            for (j = i; j > 0 && arr[j-1]>temp ; j--) {
+                System.out.printf("arr[" + (j-1) + "]=" + arr[j-1] + "\n");
+                arr[j] = arr[j-1];
             }
+            arr[j] = temp;
+            System.out.println(Arrays.toString(arr));
         }
     }
+
 
     public static void main(String[] args) {
         insertionSort s = new insertionSort();
