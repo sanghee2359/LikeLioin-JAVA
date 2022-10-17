9ca63523c34fb952b61f952ff28899a9dd59c0dd
sanghee <wjdtkdgml7352@daum.net>
2022-10-17 09:22:09 +0900

    [추가] 별찍기 메소드 생성

--- Week5/RightTriangle.java
+++ Week5/RightTriangle.java
@@ -1,17 +1,22 @@
 package Week5;
 
-public class RightTriangle {
-    public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
-        for(int i=0; i<4; i++){
-            for(int j=0; j<i+1; j++ ){
-                System.out.print("* ");
+import java.util.Scanner;
 
+public class RightTriangle {
+    public void star(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.printf("*");
+                if(i!=j) System.out.print(" ");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        RightTriangle r = new RightTriangle();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        r.star(num);
+
     }
 }
