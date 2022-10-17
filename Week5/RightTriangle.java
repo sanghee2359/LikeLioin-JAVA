4cb79ec59dca4dd1f6c8902898501d3d8c4615a8
sanghee <wjdtkdgml7352@daum.net>
2022-10-17 09:31:44 +0900

    [추가] 생성자에서 기호를 초기화

--- Week5/RightTriangle.java
+++ Week5/RightTriangle.java
@@ -3,20 +3,23 @@ package Week5;
 import java.util.Scanner;
 
 public class RightTriangle {
+    private char letter;
+    public RightTriangle(char letter){
+        this.letter = letter;
+    }
     public void star(int n){
         for(int i=0; i<n; i++){
             for(int j=0; j<i+1; j++){
-                System.out.printf("*");
+                System.out.printf("%c", letter);
                 if(i!=j) System.out.print(" ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        RightTriangle r = new RightTriangle();
+        RightTriangle r = new RightTriangle('@');
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         r.star(num);
-
     }
 }
