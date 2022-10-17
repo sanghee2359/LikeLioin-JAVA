fce1e4c36304345b6bca3739cb5dc982c4891563
sanghee <wjdtkdgml7352@daum.net>
2022-10-17 10:24:07 +0900

    [추가] 공백, 별찍기 반복문

---
+++ Week5/starPyramid.java
@@ -0,0 +1,23 @@
+package Week5;
+
+public class starPyramid {
+    // 피라미드 = *의 위치도 신경써야 합니다.
+
+    public void star(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++) {  // 공백(위치 조정)
+                System.out.print(" ");
+
+            }
+            for(int j=0; j<i+1; j++){   // 별 개수만큼 출력
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        starPyramid s = new starPyramid();
+        s.star(5);
+
+    }
+}
