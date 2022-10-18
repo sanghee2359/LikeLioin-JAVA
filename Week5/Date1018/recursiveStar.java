2a5806d410231ddc3e9bd373a7582cf3c865354a
sanghee <wjdtkdgml7352@daum.net>
2022-10-18 09:44:00 +0900

    [수정] 재귀함수 - 별찍기

---
+++ Week5/Date1018/recursiveStar.java
@@ -0,0 +1,15 @@
+package Week5.Date1018;
+
+public class recursiveStar {
+    public void triangl(int x){
+        if(x==0) return;
+        else{
+            triangl(x-1);
+            System.out.println("*".repeat(x));
+        }
+    }
+
+    public static void main(String[] args) {
+        new recursiveStar().triangl(5);
+    }
+}
