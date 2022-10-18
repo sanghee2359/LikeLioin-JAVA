9091ea8ba4f7ea6195530fba9b48fccb44f85ee1
sanghee <wjdtkdgml7352@daum.net>
2022-10-18 09:33:37 +0900

    [수정] 재귀함수 매개변수 변경

--- Week5/Date1018/CodeUp_1859.java
+++ Week5/Date1018/CodeUp_1859.java
@@ -1,15 +1,15 @@
 package Week5.Date1018;
 
 public class CodeUp_1859 {
-    public void trinagle(int x){
+    public void trinagle(int x, String now){
         if(x==0) return;
         else{
-            trinagle(x-1);
-            System.out.println("*".repeat(x));
+            System.out.println(now);
+            trinagle(x-1, now+"*");
         }
     }
 
     public static void main(String[] args) {
-        new CodeUp_1859().trinagle(5);
+        new CodeUp_1859().trinagle(5,"*");
     }
 }
