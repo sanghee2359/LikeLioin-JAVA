129e578f51634ccb7eea73fb2be0543de27e0e6b
sanghee <wjdtkdgml7352@daum.net>
2022-11-07 10:34:24 +0900

    나누어 떨어지는 숫자 배열 - ArrayList 풀이

---
+++ Week8/DividedArr2.java
@@ -0,0 +1,31 @@
+package Week8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class DividedArr2 {
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            }
+        }
+//        System.out.println(Q);
+        if(list.size() == 0) return new int[]{-1};
+        // list를 array로 자료구조 변경
+        int [] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+    public static void main(String[] args) {
+        DividedArr T = new DividedArr();
+        int[] arr = {5, 9, 7, 10};
+        System.out.println(Arrays.toString(T.solution(arr, 5)));
+    }
+}
