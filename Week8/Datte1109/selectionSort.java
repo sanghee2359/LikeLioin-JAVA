752c110822142d112584b61be46fffe11bcf3528
sanghee <wjdtkdgml7352@daum.net>
2022-11-09 09:30:20 +0900

    swap 수정

--- Week8/Datte1109/selectionSort.java
+++ Week8/Datte1109/selectionSort.java
@@ -1,23 +1,30 @@
 package Week8.Datte1109;
 
+import java.util.Arrays;
+
 public class selectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        System.out.println(Arrays.toString(arr));
+
         int i = 0;
         for (int j = 1; j < arr.length; j++) {
             if(arr[j]<arr[i]) {
                 int tmp = arr[j];
-                arr[i] = arr[j];
-                arr[j] = tmp;
+                arr[j] = arr[i];
+                arr[i] = tmp;
             }
         }
+        System.out.println(Arrays.toString(arr));
         i = 1;
         for (int j = 1; j < arr.length; j++) {
             if(arr[j]<arr[i]) {
                 int tmp = arr[j];
-                arr[i] = arr[j];
-                arr[j] = tmp;
+                arr[j] = arr[i];
+                arr[i] = tmp;
             }
         }
+        System.out.println(Arrays.toString(arr));
+
     }
 }
