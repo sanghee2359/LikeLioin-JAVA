09d024c32a29306616a5ec86381fa96be01a24e1
sanghee <wjdtkdgml7352@daum.net>
2022-11-09 09:50:12 +0900

    selection 오름차순 정렬 완성

--- Week8/Datte1109/selectionSort.java
+++ Week8/Datte1109/selectionSort.java
@@ -5,26 +5,21 @@ import java.util.Arrays;
 public class selectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        System.out.println(Arrays.toString(arr));
+        int minIdx = 0, tmp = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            minIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
 
-        int i = 0;
-        for (int j = 1; j < arr.length; j++) {
-            if(arr[j]<arr[i]) {
-                int tmp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = tmp;
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
             }
-        }
-        System.out.println(Arrays.toString(arr));
-        i = 1;
-        for (int j = 1; j < arr.length; j++) {
-            if(arr[j]<arr[i]) {
-                int tmp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = tmp;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+            tmp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = tmp;
 
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
+
