2b05ff6df0a6d046d4850313ac0742a8d03db7eb
sanghee <wjdtkdgml7352@daum.net>
2022-11-11 09:42:50 +0900

    10진수를 2진수로 전환하기

---
+++ Week8/Date1111/secretMap.java
@@ -0,0 +1,34 @@
+package Week8.Date1111;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class secretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+    String getBinaryString(int decNum){
+        String answer = "";
+        while(decNum > 0){
+            answer += decNum%2;
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
+
+        String binaryNum = "";
+
+//        for (int i = 0; i < n; i++) {
+//            binaryNum = String.valueOf(i%2) + binaryNum;
+//            arr1[i] = Integer.parseInt(binaryNum);
+//        }
+
+    }
+}
