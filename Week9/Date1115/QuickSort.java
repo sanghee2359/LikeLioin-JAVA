36f58830181d4d56ed2a273f4b49148b2131cf4f
sanghee <wjdtkdgml7352@daum.net>
2022-11-15 10:32:49 +0900

    [퀵정렬] 두번째 로직 구현 - left List, mid List, right List에 값을 담기 - 세 리스트를 하나로 병합하는 merge()메소드 생성

--- Week9/Date1115/QuickSort.java
+++ Week9/Date1115/QuickSort.java
@@ -5,31 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-    static List<Integer> left;
-    static List<Integer> right;
+    public List<Integer> merge(List<Integer>left, List<Integer>mid, List<Integer>right){
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(right);
+        answer.addAll(mid);
+        return answer;
+    }
+    public List<Integer> sort(List<Integer> arr){
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        // 1. 기준값 뽑는 로직 구현
+        int pivot = arr.size()/2;
 
-    public List<Integer> solution(int pivot, int[] arr){
         // 2. 기준값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
-        for(int i = pivot-1 ; i >= 0; i--){
-            if(arr[i] > arr[pivot]) {
-                right.add(arr[i]);
-            }else left.add(arr[i]);
+        for(int i = 0 ; i < arr.size(); i++){
+            if(arr.get(i) > pivot) right.add(arr.get(i));
+            else if(arr.get(i) < pivot) left.add(arr.get(i));
+            else mid.add(arr.get(i));
         }
-        for (int i = pivot+1; i < arr.length; i++) {
-            if(arr[i] < arr[pivot]) {
-                left.add(arr[i]);
-            }else right.add(arr[i]);
-        }
-        System.out.println(left);
-        System.out.println(right);
+
+//        System.out.println(left);
+//        System.out.println(right);
+        return merge(sort(left),sort(mid),sort(right));
     }
     public static void main(String[] args) {
-        left = new ArrayList<>();
-        right = new ArrayList<>();
-        int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
-        // 1. 기준값 뽑는 로직 구현
-        int pivot = arr.length/2;
-        System.out.println(arr[pivot]);
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < al.size(); i++) {
+            al.add(arr[i]);
+        }
+        QuickSort qs = new QuickSort();
+        qs.sort(al);
+        System.out.println(qs.sort(al));
 
     }
 }
