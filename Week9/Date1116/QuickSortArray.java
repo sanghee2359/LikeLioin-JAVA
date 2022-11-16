9d1bd773df5f15e5da1c737f70451728f8a3789f
sanghee <wjdtkdgml7352@daum.net>
2022-11-16 10:23:25 +0900

    [퀵정렬] leftidx, rightidx를 이동하며 위치를 바꿔야할 값을 찾음

--- Week9/Date1116/QuickSortArray.java
+++ Week9/Date1116/QuickSortArray.java
@@ -1,18 +1,39 @@
 package Week9.Date1116;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class QuickSortArray {
     public static void main(String[] args) {
         var arr = new int[] {20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = arr[arr.length]/2];
+        int pivot = arr[arr.length/2];
         System.out.println(pivot);
         // left index는 0부터 시작한다.
         int leftidx = 0;
         int rightidx = arr.length-1;
-        while(leftidx <= rightidx){
+        System.out.println(Arrays.toString(arr));
 
-            if(arr[leftidx] < pivot) leftidx++;
-            else if(arr[rightidx] > pivot) rightidx--;
+        /*while(true){
 
-        }
+            if(arr[leftidx] < pivot) leftidx++; // pivot 왼쪽의 값들이 pivot보다 작으면 leftindex만 올라간다
+            else {
+                rightidx--;
+                System.out.println(leftidx);
+                int tmp = arr[leftidx];
+                arr[leftidx] = arr[rightidx];
+                arr[rightidx] = tmp;
+            }
+            leftidx++;
+            rightidx--;
+            if(leftidx > rightidx) break;
+        }*/
+
+        while(arr[leftidx] < pivot) leftidx += 1;
+        while(arr[rightidx] > pivot) rightidx -= 1;
+
+        int tmp = arr[leftidx];
+        arr[leftidx] = arr[rightidx];
+        arr[rightidx] = tmp;
+        System.out.println(Arrays.toString(arr));
     }
 }
