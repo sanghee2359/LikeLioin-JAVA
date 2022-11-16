e319b94c8129a5ad78fe2dc88f77820505d5ebf4
sanghee <wjdtkdgml7352@daum.net>
2022-11-16 10:28:40 +0900

    [퀵정렬] 값을 교환, 남은 정렬을 위해 leftIdx++, rightIdx--를 한다

--- Week9/Date1116/QuickSortArray.java
+++ Week9/Date1116/QuickSortArray.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 
 public class QuickSortArray {
     public static void main(String[] args) {
-        var arr = new int[] {20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = arr[arr.length/2];
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        int pivot = arr[arr.length / 2];
         System.out.println(pivot);
         // left index는 0부터 시작한다.
         int leftidx = 0;
-        int rightidx = arr.length-1;
+        int rightidx = arr.length - 1;
         System.out.println(Arrays.toString(arr));
 
         /*while(true){
@@ -28,12 +28,17 @@ public class QuickSortArray {
             if(leftidx > rightidx) break;
         }*/
 
-        while(arr[leftidx] < pivot) leftidx += 1;
-        while(arr[rightidx] > pivot) rightidx -= 1;
+        while (arr[leftidx] < pivot) leftidx += 1;
+        while (arr[rightidx] > pivot) rightidx -= 1;
+        System.out.printf("교환 전 ,arr[4]:%d arr[7]:%d\n", arr[leftidx], arr[rightidx]);
 
         int tmp = arr[leftidx];
         arr[leftidx] = arr[rightidx];
         arr[rightidx] = tmp;
-        System.out.println(Arrays.toString(arr));
+        System.out.printf("교환 후 ,arr[4]:%d arr[7]:%d\n", arr[leftidx],arr[rightidx]);
+
+        leftidx += 1;
+        rightidx -= 1;
+//        System.out.println(Arrays.toString(arr));}
     }
 }
