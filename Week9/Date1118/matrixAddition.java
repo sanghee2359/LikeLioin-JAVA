7c176edb1733badd8229950087a32567de622add
sanghee <wjdtkdgml7352@daum.net>
2022-11-18 10:44:31 +0900

    행렬의 덧셈 문제 풀이

---
+++ Week9/Date1118/matrixAddition.java
@@ -0,0 +1,23 @@
+package Week9.Date1118;
+
+import java.util.Arrays;
+
+public class matrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+            System.out.println(Arrays.toString(answer[i]));
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        matrixAddition T = new matrixAddition();
+        int[][] arr1 = new int[][]{{1,2},{2,3}};
+        int[][] arr2 = new int[][]{{3,4},{5,6}};
+        T.solution(arr1,arr2);
+    }
+}
