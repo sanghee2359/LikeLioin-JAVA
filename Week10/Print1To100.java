5d9632ba5c6ada043f5ae91fc3807e2cbe5f4ac6
sanghee <wjdtkdgml7352@daum.net>
2022-11-22 09:11:22 +0900

    [재귀함수]1부터 100까지 출력

---
+++ Week10/Print1To100.java
@@ -0,0 +1,18 @@
+package Week10;
+
+public class Print1To100 {
+    public static void print1To100(int n){
+        if(n == 100) {
+            System.out.println(n);
+            return ;
+        }
+        else {
+            System.out.println(n);
+            print1To100(n+1);
+        }
+    }
+    public static void main(String[] args) {
+        Print1To100 T = new Print1To100();
+        print1To100(1);
+    }
+}
