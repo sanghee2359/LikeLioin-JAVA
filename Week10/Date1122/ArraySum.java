3f0d921143c51b8f73e3a9fb0ac5088bf9296159
sanghee <wjdtkdgml7352@daum.net>
2022-11-22 10:15:36 +0900

    [재귀함수] array의 모든 값 더하기 - stack over flow

--- Week10/Date1122/ArraySum.java
+++ Week10/Date1122/ArraySum.java
@@ -1,13 +1,22 @@
 package Week10.Date1122;
 // array말고 list를 쓴 이유를 생각 해보세요.
 // 값을 꺼내면서 list에 값이 없어지면? 탈출조건을 만들 수 있기 때문일 것 같다
-
+// 탈출 조건이 있으나 탈출 조건에 도달하지 못하면 stack overFlow가 발생하기도 한다.
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArraySum {
     static int sum = 0;
     public static int sum(List<Integer> nums){
+        if(nums.size() == 0) {
+            return 0;
+        }
+        else {
+            int idx = nums.size() - 1;
+            sum += nums.get(idx);
+            System.out.println(sum);
+            return sum(nums);
+        }
     }
 
     public static void main(String[] args) {
