8fd7450420839769ab6552a997676b44d00fe7bd
sanghee <wjdtkdgml7352@daum.net>
2022-11-25 10:25:32 +0900

    [재귀 함수] 재귀함수 작동방식 이해하기 - 순서 작성

--- Week10/Date1125/Recur.java
+++ Week10/Date1125/Recur.java
@@ -2,13 +2,19 @@ package Week10.Date1125;
 
 public class Recur {
     //recur(3)과 recur(4)의 결과를 실행해보지 않고 출력 해보세요.
-    public static void recur(int n) {
-        if (n <= 0) return;
+    public static void recur(int n) { // n = 3
+        if (n <= 0) return; // 0 이하면 종료
         recur(n - 1);
-        System.out.println(n);
-        recur(n - 2);
+        System.out.println(n); // 1 실행
+        recur(n - 2); //
     }
     public static void main(String[] args) {
         recur(3);
+        // n=3, recur(2) -> n=2, recur(1) -> n=1, print(1), recur(1) -> 종료조건 도달 -> recur(1) 종료 -> print(2) -> 종료조건 -> print(3) -> n = 3-1, recur(1) -> print(1)
+        // 결과
+        // 1
+        // 2
+        // 3
+        // 1
     }
 }
